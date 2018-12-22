49fb1f4fbc04822755c5735910dfc6f31dcd4b09
Qhkm92 <replace-all>
2018-12-22 23:20:16 +0900

    Fixed typo

--- main.go
+++ main.go
@@ -32,16 +32,19 @@ func GetPeople(w http.ResponseWriter, r *http.Request) {
 func GetPerson(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, item := range people {
-	if item.ID == params["id"]
+	if item.ID == params["id"] {
+		json.NewEncoder(w).Encode(item)
+		return
+		}
 	}
+	json.NewEncoder(w).Encode(&Person{})
 }
 
 //Create Person
-//
 func CreatePerson(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	var person Person
-	_ = json.NewEncoder(r.body).Decode(&person)
+	_ = json.NewDecoder(r.Body).Decode(&person)
 	person.ID = params["id"]
 	people = append(people, person)
 	json.NewEncoder(w).Encode(people)
@@ -52,7 +55,7 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range people {
 		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1]...)
+			people = append(people[:index], people[index+1:]...)
 			break
 		}
 		json.NewEncoder(w).Encode(people)
